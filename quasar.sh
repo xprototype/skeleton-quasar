ed47068c3c3df881c79dd68a570b0cae161aea9e
William Correa <wilcorrea@gmail.com>
2019-03-20 20:57:05 -0300

    [#10/fix] Fix sintax of if in quasar.sh

--- quasar.sh
+++ quasar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -d /var/www/app/node_modules ]; then
+if [[ ! -d /var/www/app/node_modules ]]; then
   echo "~> installing dependencies"
   if yarn install 2>/dev/null; then
      yarn install
@@ -9,7 +9,7 @@ if [ ! -d /var/www/app/node_modules ]; then
   fi
 fi
 
-if [ ! -f /home/node/bin/node ]; then
+if [[ ! -f /home/node/bin/node ]]; then
   echo "~> export bin"
   cp /usr/local/bin/node /home/node/bin/node
   echo "~> fix permissions"
