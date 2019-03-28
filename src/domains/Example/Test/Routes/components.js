76074a447b6f73b09164e0ba357d395cb6bbe329
William Correa <wilcorrea@gmail.com>
2019-03-28 19:33:39 -0300

    [#4/feature] Create new routes to hooks examples

---
+++ src/domains/Example/Test/Routes/components.js
@@ -0,0 +1,19 @@
+/**
+ * @returns {Promise}
+ */
+export const testTable = () => import('src/view/Dashboard/Example/Test/TestTable')
+
+/**
+ * @returns {Promise}
+ */
+export const testForm = () => import('src/view/Dashboard/Example/Test/TestForm')
+
+/**
+ * @returns {Promise}
+ */
+export const testWithHooksTable = () => import('src/view/Dashboard/Example/TestWithHooks/TestWithHooksTable')
+
+/**
+ * @returns {Promise}
+ */
+export const testWithHooksForm = () => import('src/view/Dashboard/Example/TestWithHooks/TestWithHooksForm')
