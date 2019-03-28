51acf729f1ad93ac15305c344c1fe3b3373bcedd
William Correa <wilcorrea@gmail.com>
2019-03-28 19:33:54 -0300

    [#4/feature] Create new views to hooks examples

---
+++ src/view/Dashboard/Example/TestWithHooks/TestWithHooksForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <prototype-form v-bind="bind" />
+</template>
+
+<script type="text/javascript">
+import PrototypeView from 'src/app/Prototype/View/Prototype'
+import TestWithHooks from 'src/domains/Example/Test/Prototype/TestWithHooks'
+
+/**
+ * @typedef {TestWithHooksForm}
+ */
+export default {
+  /**
+   */
+  extends: PrototypeView,
+  /**
+   */
+  name: 'TestWithHooksForm',
+  /**
+   */
+  prototype: TestWithHooks
+}
+</script>
+
+<style
+  lang="stylus"
+  rel="stylesheet/stylus"
+  scoped
+>
+</style>
